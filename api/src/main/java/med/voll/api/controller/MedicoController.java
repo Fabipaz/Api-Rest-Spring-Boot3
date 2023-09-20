0ebcc67b65f516ffe7326166e987c1dc4e9fe7ae
Hector Pastrana <fabipaz2803@gmail.com>
2023-09-19 20:54:44 -0500

    Agregando metodo PUT (actualizar)

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,13 +2,11 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListadoMedico;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,4 +26,11 @@ public class MedicoController {
         return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
 
     }
+    @PutMapping
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico){
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actualizarDatos(datosActualizarMedico);
+    }
+
 }
